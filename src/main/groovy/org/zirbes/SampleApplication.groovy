8d1b276e7f062baac46134f5eba4b66eeb65ed2b
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-07-28 11:32:31 -0500

    Initial commit

---
+++ src/main/groovy/org/zirbes/SampleApplication.groovy
@@ -0,0 +1,29 @@
+package org.zirbes
+
+import org.joda.time.LocalDate
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+class SampleApplication {
+
+	@RequestMapping("/")
+	def helloWorld() {
+        LocalDate today = new LocalDate()
+
+		[message: "Hello Gr8conf", author: "Spring Boot", timestamp: today]
+	}
+
+	static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleApplication.class, args)
+	}
+
+}
